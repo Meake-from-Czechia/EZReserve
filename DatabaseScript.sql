8346c5616d91e236e2301177090c3eb2002cfb62
meake <meake@KEKESH-DESKTOP>
2024-11-25 14:03:51 +0100

    Added database creation script for mysql.

---
+++ DatabaseScript.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE `Customers` (
+  `CustomerId` int(11) NOT NULL,
+  `FirstName` varchar(50)  NOT NULL,
+  `LastName` varchar(50)  NOT NULL,
+  `Email` varchar(255)  NOT NULL,
+  `StreetAddress` varchar(255)  NOT NULL,
+  `City` varchar(255)  NOT NULL,
+  `PostalCode` varchar(20)  NOT NULL
+) ;
+
+CREATE TABLE `Reservations` (
+  `ReservationId` int(11) NOT NULL,
+  `CustomerId` int(11) NOT NULL,
+  `Room` varchar(50)  NOT NULL,
+  `Reservation_From` datetime NOT NULL,
+  `Reservation_To` datetime NOT NULL
+);
+
+ALTER TABLE `Customers`
+  ADD PRIMARY KEY (`CustomerId`);
+
+ALTER TABLE `Reservations`
+  ADD PRIMARY KEY (`ReservationId`),
+  ADD KEY `FK_Reservations_CustomerId` (`CustomerId`);
+
+
+ALTER TABLE `Customers`
+  MODIFY `CustomerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+ALTER TABLE `Reservations`
+  MODIFY `ReservationId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+
+
+ALTER TABLE `Reservations`
+  ADD CONSTRAINT `FK_Reservations_CustomerId` FOREIGN KEY (`CustomerId`) REFERENCES `Customers` (`CustomerId`) ON DELETE CASCADE ON UPDATE NO ACTION;
